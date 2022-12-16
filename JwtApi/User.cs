2ca89bed0101b9bcca3bb3e66f132c16d71bb1e6
Vinicius Politta <vinicius.politta1@gmail.com>
2022-12-16 18:40:15 -0300

    Adicionar arquivos de projeto.

---
+++ JwtApi/User.cs
@@ -0,0 +1,9 @@
+﻿namespace JwtApi
+{
+    public class User
+    {
+        public string Username { get; set; } = string.Empty;
+        public byte[] PasswordSalt { get; set; } 
+        public byte[] PasswordHash { get; set; } 
+    }
+}
